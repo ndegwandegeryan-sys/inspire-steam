846afc8439aff1673e20760e421469034b3f9fd5
ndegwandegeryan-sys <ndegwandegeryan@gmail.com>
2026-02-11 09:30:37 +0300

    first commit

---
+++ Challenge 1.py
@@ -0,0 +1,8 @@
+university = "JKUAT"
+grade = "B"
+favourite_course = "Software-engineer"
+cluster_points = "65"
+print (f"i want to go to {university} university")
+print (f"i got a {grade} in my kcse")
+print (f"my favourite course is {favourite_course}")
+print (f"i got {cluster_points} points")
---
+++ hello.py
@@ -0,0 +1,12 @@
+#This is a comment
+print("Hello from Ryan")
+#city
+print("I am from Kiambu")
+#hobbies
+print("i like to sleep,eat,play football,game and watch movies")
+Age = 24
+Height = 1.75
+Weight = 55
+print(f"I am {Age} years old")
+print(f"I am also {Height} meters tall")
+print(f"And weigh {Weight} kgs")
